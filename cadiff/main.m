95666a16f3c20d9d06a8c98faea4de6385749d24
Wade Tregaskis <wadetregaskis@mac.com>
2013-09-24 23:37:56 -0700

    Actually do the comparison now.  This works, but performance is a problem; allowing all I/Os simultaneously fails a bunch of them with error 24 (EMFILE), so I’m having to limit them and I’m not getting the concurrency I expect (or that I had when allowing it to be unlimited and simply keeping the number of input files sufficiently low).  Hmmm.

--- cadiff/main.m
+++ cadiff/main.m
@@ -32,10 +32,10 @@ static BOOL computeHashes(NSURL *files,
                           dispatch_queue_t updateQueue,
                           dispatch_group_t dispatchGroup) NOT_NULL(1, 2, 3) {
     NSDirectoryEnumerator *fileEnumerator
-        = [[NSFileManager defaultManager] enumeratorAtURL:files
-                               includingPropertiesForKeys:nil
-                                                  options:NSDirectoryEnumerationSkipsHiddenFiles
-                                             errorHandler:^(NSURL *url, NSError *error) {
+        = [NSFileManager.defaultManager enumeratorAtURL:files
+                             includingPropertiesForKeys:nil
+                                                options:NSDirectoryEnumerationSkipsHiddenFiles
+                                           errorHandler:^(NSURL *url, NSError *error) {
         fprintf(stderr, "Error while enumerating files in \"%s\": %s\n", url.path.UTF8String, error.localizedDescription.UTF8String);
         return YES;
     }];
@@ -46,6 +46,8 @@ static BOOL computeHashes(NSURL *files,
     }
 
     BOOL allGood = YES;
+    dispatch_semaphore_t concurrencyLimiter = dispatch_semaphore_create(2);
+    dispatch_queue_t jobQueue = dispatch_queue_create("Job Queue", DISPATCH_QUEUE_SERIAL);
 
     for (NSURL *file in fileEnumerator) {
         dispatch_io_t fileIO = dispatch_io_create_with_path(DISPATCH_IO_STREAM,
@@ -83,51 +85,57 @@ static BOOL computeHashes(NSURL *files,
         }
 
         dispatch_group_enter(dispatchGroup);
-        dispatch_io_read(fileIO,
-                         0,
-                         SIZE_MAX,
-                         dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0),
-                         ^(bool done, dispatch_data_t data, int error) {
-                             if (0 == error) {
-                                 dispatch_data_apply(data,
-                                                     ^bool(dispatch_data_t region,
-                                                           size_t offset,
-                                                           const void *buffer,
-                                                           size_t size) {
-                                                         if (1 == SHA1_Update(hashContext, buffer, size)) {
-                                                             return true;
-                                                         } else {
-                                                             fprintf(stderr, "Error computing SHA1 on bytes [%zu, %zu] in \"%s\".\n", offset, offset + size - 1, file.path.UTF8String);
-                                                             return false;
-                                                         }
-                                                     });
+        dispatch_async(jobQueue, ^{
+            dispatch_semaphore_wait(concurrencyLimiter, DISPATCH_TIME_FOREVER);
+
+            dispatch_io_read(fileIO,
+                             0,
+                             SIZE_MAX,
+                             dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0),
+                             ^(bool done, dispatch_data_t data, int error) {
+                                 if (0 == error) {
+                                     dispatch_data_apply(data,
+                                                         ^bool(dispatch_data_t region,
+                                                               size_t offset,
+                                                               const void *buffer,
+                                                               size_t size) {
+                                                             if (1 == SHA1_Update(hashContext, buffer, size)) {
+                                                                 return true;
+                                                             } else {
+                                                                 fprintf(stderr, "Error computing SHA1 on bytes [%zu, %zu] in \"%s\".\n", offset, offset + size - 1, file.path.UTF8String);
+                                                                 return false;
+                                                             }
+                                                         });
+
+                                     if (done) {
+                                         unsigned char hash[SHA_DIGEST_LENGTH];
+
+                                         if (1 == SHA1_Final(hash, hashContext)) {
+                                             NSData *hashAsData = [NSData dataWithBytes:hash length:SHA_DIGEST_LENGTH];
+
+                                             printf("Hash for \"%s\" is %s.\n", file.path.UTF8String, hashAsData.description.UTF8String);
+                                             fflush(stdout);
+
+                                             dispatch_sync(updateQueue, ^{
+                                                 URLsToHashes[file] = hashAsData;
+                                                 hashesToURLs[hashAsData] = file;
+                                             });
+                                         } else {
+                                             fprintf(stderr, "Unable to conclude SHA1 of \"%s\".\n", file.path.UTF8String);
+                                         }
+                                     }
+                                 } else {
+                                     fprintf(stderr, "Error %d while reading from \"%s\".\n", error, file.path.UTF8String);
+                                 }
 
                                  if (done) {
-                                     unsigned char hash[SHA_DIGEST_LENGTH];
-
-                                     if (1 == SHA1_Final(hash, hashContext)) {
-                                         NSData *hashAsData = [NSData dataWithBytes:hash length:SHA_DIGEST_LENGTH];
-
-                                         printf("Hash for \"%s\" is %s.\n", file.path.UTF8String, hashAsData.description.UTF8String);
-
-                                         dispatch_async(updateQueue, ^{
-                                             URLsToHashes[file] = hashAsData;
-                                             hashesToURLs[hashAsData] = file;
-                                         });
-                                     } else {
-                                         fprintf(stderr, "Unable to conclude SHA1 of \"%s\".\n", file.path.UTF8String);
-                                     }
+                                     //dispatch_io_close(fileIO, DISPATCH_IO_STOP);
+                                     free(hashContext);
+                                     dispatch_group_leave(dispatchGroup);
+                                     dispatch_semaphore_signal(concurrencyLimiter);
                                  }
-                             } else {
-                                 fprintf(stderr, "Error %d while reading from \"%s\".\n", error, file.path.UTF8String);
-                             }
-
-                             if (done) {
-                                 //dispatch_io_close(fileIO, DISPATCH_IO_STOP);
-                                 free(hashContext);
-                                 dispatch_group_leave(dispatchGroup);
-                             }
-                         });
+                             });
+        });
     }
 
     return allGood;
@@ -168,27 +176,89 @@ int main(int argc, char* const argv[]) {
         NSMutableDictionary *aURLsToHashes = [NSMutableDictionary dictionary];
         NSMutableDictionary *bURLsToHashes = [NSMutableDictionary dictionary];
 
-        dispatch_group_t dispatchGroupA = dispatch_group_create();
-        dispatch_group_t dispatchGroupB = dispatch_group_create();
+        dispatch_group_t dispatchGroup = dispatch_group_create();
 
         dispatch_queue_t updateQueue = dispatch_queue_create("Aggregation Queue", DISPATCH_QUEUE_SERIAL);
 
-        if (!computeHashes(a, aURLsToHashes, aHashesToURLs, updateQueue, dispatchGroupA)) {
+        if (!computeHashes(a, aURLsToHashes, aHashesToURLs, updateQueue, dispatchGroup)) {
             return -1;
         }
 
-        if (!computeHashes(b, bURLsToHashes, bHashesToURLs, updateQueue, dispatchGroupB)) {
+        if (!computeHashes(b, bURLsToHashes, bHashesToURLs, updateQueue, dispatchGroup)) {
             return -1;
         }
 
-        dispatch_group_wait(dispatchGroupA, DISPATCH_TIME_FOREVER);
-        dispatch_group_wait(dispatchGroupB, DISPATCH_TIME_FOREVER);
+        dispatch_group_wait(dispatchGroup, DISPATCH_TIME_FOREVER);
 
         printf("Calculated %lu hashes for \"%s\", and %lu for \"%s\".\n",
                (unsigned long)aHashesToURLs.count,
                a.path.UTF8String,
                (unsigned long)bHashesToURLs.count,
                b.path.UTF8String);
+
+        NSMutableDictionary *duplicates = [NSMutableDictionary dictionary];
+        NSMutableSet *onlyInA = [NSMutableSet set];
+        NSMutableSet *onlyInB = [NSMutableSet set];
+
+        [aURLsToHashes enumerateKeysAndObjectsUsingBlock:^(NSURL *file, NSData *hash, BOOL *stop) {
+            NSURL *duplicateFile = bHashesToURLs[hash];
+
+            if (duplicateFile) {
+                if ([NSFileManager.defaultManager contentsEqualAtPath:file.path andPath:duplicateFile.path]) {
+                    duplicates[file] = duplicateFile;
+                } else {
+                    printf("False positive between \"%s\" and \"%s\".\n", file.path.UTF8String, duplicateFile.path.UTF8String);
+                }
+            } else {
+                [onlyInA addObject:file];
+            }
+        }];
+
+        [bURLsToHashes enumerateKeysAndObjectsUsingBlock:^(NSURL *file, NSData *hash, BOOL *stop) {
+            NSURL *duplicateFile = aHashesToURLs[hash];
+
+            if (!duplicateFile) {
+                [onlyInB addObject:file];
+            }
+        }];
+
+        printf("\n");
+
+        if (0 < duplicates.count) {
+            printf("Duplicates:\n");
+
+            [duplicates enumerateKeysAndObjectsUsingBlock:^(NSURL *aVersion, NSURL *bVersion, BOOL *stop) {
+                printf("\t%s <-> %s\n", aVersion.path.UTF8String, bVersion.path.UTF8String);
+            }];
+
+            printf("\n");
+        } else {
+            printf("No duplicates.\n");
+        }
+
+        if (onlyInA.anyObject) {
+            printf("Only in \"%s\":\n", a.path.UTF8String);
+
+            [onlyInA enumerateObjectsUsingBlock:^(NSURL *file, BOOL *stop) {
+                printf("\t%s\n", file.path.UTF8String);
+            }];
+
+            printf("\n");
+        } else {
+            printf("Nothing unique to \"%s\".\n", a.path.UTF8String);
+        }
+
+        if (onlyInB.anyObject) {
+            printf("Only in \"%s\":\n", b.path.UTF8String);
+
+            [onlyInB enumerateObjectsUsingBlock:^(NSURL *file, BOOL *stop) {
+                printf("\t%s\n", file.path.UTF8String);
+            }];
+
+            printf("\n");
+        } else {
+            printf("Nothing unique to \"%s\".\n", b.path.UTF8String);
+        }
     }
 
     return 0;
