56979572674301fd85def1552b948471fea27b36
Wade Tregaskis <wadetregaskis@mac.com>
2013-09-24 21:27:49 -0700

    Initial Commit

---
+++ cadiff/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  cadiff
+//
+//  Created by Wade Tregaskis on 24/09/13.
+//  Copyright (c) 2013 Wade Tregaskis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
